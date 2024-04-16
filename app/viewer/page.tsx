f881a0f51ff816bca6c20f2c686ff87890df4c58
Arnav Varshney <varshney.arnav@gmail.com>
2024-04-17 00:58:41 +0800

    Fix SSR
    Signed-off-by: Arnav Varshney <varshney.arnav@gmail.com>

--- app/viewer/page.tsx
+++ app/viewer/page.tsx
@@ -1,11 +1,14 @@
-'use client';
-
 import React from 'react';
 import '../globals.css';
 
 import {ToastContainer} from "react-toastify";
 import Link from 'next/link';
-import {QRCodeScanner} from "../components/qrcode_scanner";
+import dynamic from "next/dynamic";
+
+const DynamicQRCodeScanner = dynamic(
+    () => import('../components/qrcode_scanner').then((mod) => mod.QRCodeScanner),
+    {ssr: false}
+);
 
 export default function Viewer() {
     // @ts-ignore
@@ -14,7 +17,7 @@ export default function Viewer() {
             <h1>Image Viewer</h1>
             <p>Scan the QR code to view the image</p>
             <div style={{width: 300}}>
-                <QRCodeScanner/>
+                <DynamicQRCodeScanner/>
                 <div className="button-container" style={{marginTop: 24, justifyContent: "center"}}>
                     <Link href="/">
                         <label className='button-style'>
